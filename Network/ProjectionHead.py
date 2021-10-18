a6576597e50c48a5968f505aca71171f9104a767
Armanfard-Lab <83035307+Armanfard-Lab@users.noreply.github.com>
2021-10-18 15:53:04 -0400

    Create ProjectionHead.py

---
+++ Network/ProjectionHead.py
@@ -0,0 +1,19 @@
+class Projection(nn.Module):
+    def __init__(self, input_dim=512, hidden_dim=512, output_dim=128):
+        super().__init__()
+        self.output_dim = output_dim
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+
+        self.model = nn.Sequential(
+            nn.Linear(self.input_dim, self.hidden_dim, bias=True),
+            nn.BatchNorm1d(self.hidden_dim),
+            nn.ReLU(),
+            #nn.Linear(self.hidden_dim, self.hidden_dim, bias=True),
+            #nn.BatchNorm1d(self.hidden_dim),
+            #nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.output_dim, bias=False))
+
+    def forward(self, x):
+        x = self.model(x)
+        return F.normalize(x, dim=1)
