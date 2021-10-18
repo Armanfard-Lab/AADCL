e38055fc7204a930947079faf3e63b049ff0fe47
Armanfard-Lab <83035307+Armanfard-Lab@users.noreply.github.com>
2021-10-18 15:59:26 -0400

    Update and rename MelSpec.py to time_stretch.py

--- Transform/MelSpec.py
+++
@@ -1,4 +0,0 @@
-def MelSpec(y,sr):
-  S = torchaudio.transforms.MelSpectrogram(sample_rate= sr, n_fft=2048, hop_length=512, f_max=sr/2 ,norm='slaney',mel_scale='slaney')(y)
-  S_dB = torchaudio.transforms.AmplitudeToDB()(S)
-  return S_dB
