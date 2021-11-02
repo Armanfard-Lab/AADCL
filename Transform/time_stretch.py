16f2ff8640fc68f7eba556eec38e4190ddaf6d80
Armanfard-Lab <83035307+Armanfard-Lab@users.noreply.github.com>
2021-11-02 15:00:40 -0400

    Update time_stretch.py

--- Transform/time_stretch.py
+++ Transform/time_stretch.py
@@ -6,6 +6,7 @@ from torch.nn import functional as F
 from torch import Tensor
 import librosa
 import librosa.display
+from Source.utils import MelSpec
 
 
 def time_stretch(y,R_min=0.5, R_max=2, sr=16000):
